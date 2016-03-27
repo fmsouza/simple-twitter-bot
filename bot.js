7311888c4ba9cdc7b1c38a95164fb6725acdac24
Fred Souza <fredericoamsouza@gmail.com>
2016-03-27 01:19:11 -0300

    Changing retweet API method

--- bot.js
+++ bot.js
@@ -1,42 +1,29 @@
-var Twit = require('twit'),
+var Twit   = require('twit'),
     Config = require('./config.js');
 
-if (Config.debug) {
-    console.log("In debug mode.");
-}
+if (Config.debug) console.log("In debug mode.");
 
-var T = new Twit({
+let T = new Twit({
     consumer_key: Config.consumer_key,
     consumer_secret: Config.consumer_secret,
     access_token: Config.access_token,
     access_token_secret: Config.access_token_secret
 });
 
-var stream = T.stream('user', { with:'followings' } );
+let stream = T.stream('user', { with: 'followings' } );
 
 stream.on('tweet', function (tweet) {
     if (Config.valid(tweet)) {
-        var response = Config.respond(tweet);
         if (!Config.debug) {
-            T.post('statuses/update', { status: response }, function (err, reply) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    var now = new Date();
-                    console.log('[' + now.toJSON() + '] SENT: ' + response);
-                }
+            T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+                if (err) console.error(`[${err.statusCode}] ERROR ${err.code}: ${err.message}`);
+                else console.log(`[${(new Date()).toJSON()}] SENT - ${data.id}`);
             });
-        } else {
-            var now = new Date();
-            console.log('[' + now.toJSON() + '] ' + response);
-        }
-    } else {
-        var now = new Date();
-        console.log('[' + now.toJSON() + '] INVALID: @' + tweet.user.screen_name + ':' + tweet.text);
-    }
+        } else console.log(`[${(new Date()).toJSON()}] @${tweet.user.screen_name}: ${tweet.text}`);
+    } else console.log(`[${(new Date()).toJSON()}] INVALID - @${tweet.user.screen_name}: ${tweet.text}`);
 });
 
 stream.on('warning', function (item) { console.log('WARNING: ' + item); });
-stream.on('disconnect', function (item) { console.log('Stream disconnected.'); });
-stream.on('connect', function (item) { console.log('Stream connected.'); });
-stream.on('reconnect', function (item) { console.log('Stream reconnected.'); });
+stream.on('disconnect', function () { console.log('Stream disconnected.'); });
+stream.on('connect', function () { console.log('Stream connected.'); });
+stream.on('reconnect', function () { console.log('Stream reconnected.'); });
